f851c05e68cc84b491024fa33b428d9c19091170
snagypeter <snagypeter@gmail.com>
2017-03-25 20:48:23 +0100

    Container changed to Image

---
+++ common/models/image.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = function (Image) {
+//	Image.createFile = function(fileName, fileContent, callback) {
+//		var response = "Failed";
+//		
+//		Image.createContainer(,callback);
+//	};
+//
+//	Image.testDb = function (imageId, callback) {
+//		var response = "Image";
+//
+//		var containers = Image.getContainers();
+//		response += containers;
+//		
+//		callback(null, response);
+//	};
+//
+//	Image.remoteMethod("testDb", {
+//		accepts: { arg: "documentId", type: "string"},
+//		returns: {arg: "data", type: "string"}
+//	});
+	
+};
