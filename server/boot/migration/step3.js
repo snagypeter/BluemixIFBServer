e092bc67dc3226e1c6e8587bfe98edb5ac602cb7
snagypeter <snagypeter@gmail.com>
2017-05-02 21:31:43 +0200

    Adding default users to migration. Users moved to MongoDB from memory.

---
+++ server/boot/migration/step3.js
@@ -0,0 +1,27 @@
+module.exports = function (app) {
+	function runStep() {
+		console.log("Adding default users");
+
+		app.dataSources.MongoDB.automigrate("Users", function (err) {
+			if (err) {
+				throw err;
+			}
+
+			app.models.User.create([
+				{realm: "CFB", username: "admin@cfb.com", email: "admin@cfb.com", emailVerified: true, password: "1234"},
+				{realm: "CFB", username: "snp@cfb.com", email: "snp@cfb.com", emailVerified: true, password: "1234"}
+			], function (err, users) {
+				if (err) {
+					throw err;
+				}
+			});
+		});
+	}
+
+	app.migrationScripts.push({
+		stepId: 3,
+		callback: function () {
+			app.testMigration(3, runStep);
+		}
+	});
+};
